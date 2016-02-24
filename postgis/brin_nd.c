84185e2da3786d8f732ca94b875d34e0f0dbf37d
Ronan Dunklau <ronan@dunklau.fr>
2016-02-24 12:41:01 +0100

    Replace contained by contains function, working on cached bbox

--- postgis/brin_nd.c
+++ postgis/brin_nd.c
@@ -14,72 +14,61 @@
 #include <string.h>
 
 /*
-** For debugging
-*/
+ ** For debugging
+ */
 #if POSTGIS_DEBUG_LEVEL > 0
 static int geog_counter_leaf = 0;
 static int geog_counter_internal = 0;
 #endif
 
-Datum is_contained_box3d_geom(PG_FUNCTION_ARGS);
+Datum contains_box3d_geom(PG_FUNCTION_ARGS);
 
 static double BOX3D_xmin(BOX3D *box)
 {
-        return Min(box->xmin, box->xmax);
+	return Min(box->xmin, box->xmax);
 }
 
 static double BOX3D_ymin(BOX3D *box)
 {
-        return Min(box->ymin, box->ymax);
+	return Min(box->ymin, box->ymax);
 }
 
 static double BOX3D_zmin(BOX3D *box)
 {
-        return Min(box->zmin, box->zmax);
+	return Min(box->zmin, box->zmax);
 }
 
 static double BOX3D_xmax(BOX3D *box)
 {
-        return Max(box->xmin, box->xmax);
+	return Max(box->xmin, box->xmax);
 }
 
 static double BOX3D_ymax(BOX3D *box)
 {
-        return Max(box->ymin, box->ymax);
+	return Max(box->ymin, box->ymax);
 }
 
 static double BOX3D_zmax(BOX3D *box)
 {
-        return Max(box->zmin, box->zmax);
+	return Max(box->zmin, box->zmax);
 }
 
-PG_FUNCTION_INFO_V1(is_contained_box3d_geom);
-Datum is_contained_box3d_geom(PG_FUNCTION_ARGS)
+PG_FUNCTION_INFO_V1(contains_box3d_geom);
+Datum contains_box3d_geom(PG_FUNCTION_ARGS)
 {
 	/* take the first argument - a box3d */
-        BOX3D *box_a = (BOX3D *)PG_GETARG_POINTER(0);
+	BOX3D *box_a = (BOX3D *)PG_GETARG_POINTER(0);
 
-	/* take the second argument - a geometry - and retrieve its box3d */	
-        GSERIALIZED *geom = PG_GETARG_GSERIALIZED_P(1);
-        LWGEOM *lwgeom = lwgeom_from_gserialized(geom);
-        GBOX gbox;
-        BOX3D *box_b;
-        int rv = lwgeom_calculate_gbox(lwgeom, &gbox);
-
-        if ( rv == LW_FAILURE )
-                PG_RETURN_NULL();
-
-        box_b = box3d_from_gbox(&gbox);
-        box_b->srid = lwgeom->srid;
-
-        lwgeom_free(lwgeom);
-
-	/* check if the box3d is contained in the bounding box of the geometry */
-        if (BOX3D_xmin(box_a) > BOX3D_xmin(box_b) && BOX3D_ymin(box_a) > BOX3D_ymin(box_b) && BOX3D_zmin(box_a) > BOX3D_zmin(box_b)) {
-                if (BOX3D_xmax(box_a) < BOX3D_xmax(box_b) && BOX3D_ymax(box_a) < BOX3D_ymax(box_b) && BOX3D_zmax(box_a) < BOX3D_zmax(box_b)) {
-                        PG_RETURN_BOOL(TRUE);
-                }
-        }
-
-        PG_RETURN_BOOL(FALSE);
+	/* take the second argument - a geometry - and retrieve its box3d */
+	GSERIALIZED *geom = PG_GETARG_GSERIALIZED_P(1);
+	LWGEOM *lwgeom = lwgeom_from_gserialized(geom);
+	GBOX *gbox = lwgeom_get_bbox(lwgeom);
+	bool result = TRUE;
+	if((BOX3D_xmin(box_a) > gbox->xmin) || (BOX3D_xmax(box_a) < gbox->xmax) ||
+			(BOX3D_ymin(box_a) > gbox->ymin) || (BOX3D_ymax(box_a) < gbox->ymax) ||
+			(BOX3D_zmin(box_a) > gbox->zmin) || (BOX3D_zmax(box_a) < gbox->zmax)){
+		result = FALSE;
+	}
+	lwgeom_free(lwgeom);
+	PG_RETURN_BOOL(result);
 }
