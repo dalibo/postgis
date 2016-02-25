0e6b0913803c744e2e90d63eb85d2b76faf90ff1
Ronan Dunklau <ronan@dunklau.fr>
2016-02-25 18:38:41 +0100

    Cleanup

--- postgis/brin_nd.c
+++ postgis/brin_nd.c
@@ -16,105 +16,11 @@
 #include "utils/datum.h"
 #include "gserialized_gist.h"
 
-/*
- ** For debugging
- */
-#if POSTGIS_DEBUG_LEVEL > 0
-static int geog_counter_leaf = 0;
-static int geog_counter_internal = 0;
-#endif
-
-Datum contains_box3d_geom(PG_FUNCTION_ARGS);
-Datum contains_box2d_geom(PG_FUNCTION_ARGS);
-Datum is_contained_box2d_geom(PG_FUNCTION_ARGS);
-Datum overlaps_box2d_geom(PG_FUNCTION_ARGS);
-Datum gidx_brin_inclusion_add_value(BrinDesc *bdesc, BrinValues *column, Datum newval, bool isnull, int ndims);
-
-#define BOX_xmin(a) (Min(a->xmin, a->xmax))
-#define BOX_xmax(a) (Max(a->xmin, a->xmax))
-#define BOX_ymin(a) (Min(a->ymin, a->ymax))
-#define BOX_ymax(a) (Max(a->ymin, a->ymax))
-#define BOX_zmin(a) (Min(a->zmin, a->zmax))
-#define BOX_zmax(a) (Max(a->zmin, a->zmax))
-
 #define INCLUSION_UNION				0
 #define INCLUSION_UNMERGEABLE		1
 #define INCLUSION_CONTAINS_EMPTY	2
 
-#define PROCNUM_ADDVALUE_BASE		20
-
-
-PG_FUNCTION_INFO_V1(contains_box3d_geom);
-Datum contains_box3d_geom(PG_FUNCTION_ARGS)
-{
-	/* take the first argument - a box3d */
-	BOX3D *box_a = (BOX3D *)PG_GETARG_POINTER(0);
-
-	/* take the second argument - a geometry - and retrieve its box3d */
-	GSERIALIZED *geom = PG_GETARG_GSERIALIZED_P(1);
-	LWGEOM *lwgeom = lwgeom_from_gserialized(geom);
-	const GBOX *gbox = lwgeom_get_bbox(lwgeom);
-	bool result = TRUE;
-	if((BOX_xmin(box_a) > gbox->xmin) || (BOX_xmax(box_a) < gbox->xmax) ||
-			(BOX_ymin(box_a) > gbox->ymin) || (BOX_ymax(box_a) < gbox->ymax) ||
-			(BOX_zmin(box_a) > gbox->zmin) || (BOX_zmax(box_a) < gbox->zmax)){
-		result = FALSE;
-	}
-	lwgeom_free(lwgeom);
-	PG_RETURN_BOOL(result);
-}
-
-PG_FUNCTION_INFO_V1(contains_box2d_geom);
-Datum contains_box2d_geom(PG_FUNCTION_ARGS)
-{
-	GBOX *box_a = (GBOX *)PG_GETARG_POINTER(0);
-
-	/* take the second argument - a geometry - and retrieve its box3d */
-	GSERIALIZED *geom = PG_GETARG_GSERIALIZED_P(1);
-	LWGEOM *lwgeom = lwgeom_from_gserialized(geom);
-	const GBOX *gbox = lwgeom_get_bbox(lwgeom);
-	bool result = TRUE;
-	if((BOX_xmin(box_a) > gbox->xmin) || (BOX_xmax(box_a) < gbox->xmax) ||
-			(BOX_ymin(box_a) > gbox->ymin) || (BOX_ymax(box_a) < gbox->ymax)){
-		result = FALSE;
-	}
-	lwgeom_free(lwgeom);
-	PG_RETURN_BOOL(result);
-}
-
-PG_FUNCTION_INFO_V1(is_contained_box2d_geom);
-Datum is_contained_box2d_geom(PG_FUNCTION_ARGS)
-{
-	GBOX *box_a = (GBOX *)PG_GETARG_POINTER(0);
-	/* take the second argument - a geometry - and retrieve its box3d */
-	GSERIALIZED *geom = PG_GETARG_GSERIALIZED_P(1);
-	LWGEOM *lwgeom = lwgeom_from_gserialized(geom);
-	const GBOX *gbox = lwgeom_get_bbox(lwgeom);
-	bool result = TRUE;
-	if((BOX_xmin(box_a) < gbox->xmin) || (BOX_xmax(box_a) > gbox->xmax) ||
-			(BOX_ymin(box_a) < gbox->ymin) || (BOX_ymax(box_a) > gbox->ymax)){
-		result = FALSE;
-	}
-	lwgeom_free(lwgeom);
-	PG_RETURN_BOOL(result);
-}
-
-PG_FUNCTION_INFO_V1(overlaps_box2d_geom);
-Datum overlaps_box2d_geom(PG_FUNCTION_ARGS)
-{
-	GBOX *box_a = (GBOX *)PG_GETARG_POINTER(0);
-	/* take the second argument - a geometry - and retrieve its box3d */
-	GSERIALIZED *geom = PG_GETARG_GSERIALIZED_P(1);
-	LWGEOM *lwgeom = lwgeom_from_gserialized(geom);
-	const GBOX *gbox = lwgeom_get_bbox(lwgeom);
-	bool result = TRUE;
-	if((BOX_xmin(box_a) > gbox->xmax) || (BOX_xmax(box_a) < gbox->xmin) ||
-			(BOX_ymin(box_a) > gbox->ymax) || (BOX_ymax(box_a) < gbox->ymin)){
-		result = FALSE;
-	}
-	lwgeom_free(lwgeom);
-	PG_RETURN_BOOL(result);
-}
+Datum gidx_brin_inclusion_add_value(BrinDesc *bdesc, BrinValues *column, Datum newval, bool isnull, int ndims);
 
 PG_FUNCTION_INFO_V1(geom2d_brin_inclusion_add_value);
 Datum
@@ -157,7 +63,12 @@ gidx_brin_inclusion_add_value(BrinDesc *bdesc, BrinValues *column, Datum newval,
 	}
 	if (column->bv_allnulls)
 	{
-		column->bv_values[INCLUSION_UNION] = datumCopy((Datum) gidx_geom, false, GIDX_SIZE(ndims));
+		/* For clarity, set extraneous dimensions to 0 */
+		for(i=ndims; i < GIDX_NDIMS(gidx_geom); i++){
+			GIDX_SET_MIN(gidx_geom, i, (int) 0);
+			GIDX_SET_MAX(gidx_geom, i, (int) 0);
+		}
+		column->bv_values[INCLUSION_UNION] = datumCopy((Datum) gidx_geom, false, GIDX_SIZE(GIDX_NDIMS(gidx_geom)));
 		column->bv_values[INCLUSION_UNMERGEABLE] = BoolGetDatum(false);
 		column->bv_values[INCLUSION_CONTAINS_EMPTY] = BoolGetDatum(false);
 		column->bv_allnulls = false;
