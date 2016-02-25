5a345d5475678aa588b42e505cd684a1903e8dd6
Ronan Dunklau <ronan@dunklau.fr>
2016-02-25 17:43:38 +0100

    Move everything to gidx

--- postgis/brin_nd.c
+++ postgis/brin_nd.c
@@ -12,6 +12,9 @@
 #include <math.h>
 #include <float.h>
 #include <string.h>
+#include "access/brin_tuple.h"
+#include "utils/datum.h"
+#include "gserialized_gist.h"
 
 /*
  ** For debugging
@@ -25,6 +28,7 @@ Datum contains_box3d_geom(PG_FUNCTION_ARGS);
 Datum contains_box2d_geom(PG_FUNCTION_ARGS);
 Datum is_contained_box2d_geom(PG_FUNCTION_ARGS);
 Datum overlaps_box2d_geom(PG_FUNCTION_ARGS);
+Datum gidx_brin_inclusion_add_value(BrinDesc *bdesc, BrinValues *column, Datum newval, bool isnull, int ndims);
 
 #define BOX_xmin(a) (Min(a->xmin, a->xmax))
 #define BOX_xmax(a) (Max(a->xmin, a->xmax))
@@ -33,6 +37,11 @@ Datum overlaps_box2d_geom(PG_FUNCTION_ARGS);
 #define BOX_zmin(a) (Min(a->zmin, a->zmax))
 #define BOX_zmax(a) (Max(a->zmin, a->zmax))
 
+#define INCLUSION_UNION				0
+#define INCLUSION_UNMERGEABLE		1
+#define INCLUSION_CONTAINS_EMPTY	2
+
+#define PROCNUM_ADDVALUE_BASE		20
 
 
 PG_FUNCTION_INFO_V1(contains_box3d_geom);
@@ -105,4 +114,65 @@ Datum overlaps_box2d_geom(PG_FUNCTION_ARGS)
 	}
 	lwgeom_free(lwgeom);
 	PG_RETURN_BOOL(result);
+}
+
+PG_FUNCTION_INFO_V1(geom2d_brin_inclusion_add_value);
+Datum
+geom2d_brin_inclusion_add_value(PG_FUNCTION_ARGS)
+{
+	BrinDesc   *bdesc = (BrinDesc *) PG_GETARG_POINTER(0);
+	BrinValues *column = (BrinValues *) PG_GETARG_POINTER(1);
+	Datum newval = PG_GETARG_DATUM(2);
+	bool		isnull = PG_GETARG_BOOL(3);
+	PG_RETURN_DATUM(gidx_brin_inclusion_add_value(bdesc, column, newval, isnull, 2));
+}
+
+PG_FUNCTION_INFO_V1(geom3d_brin_inclusion_add_value);
+Datum
+geom3d_brin_inclusion_add_value(PG_FUNCTION_ARGS)
+{
+	BrinDesc   *bdesc = (BrinDesc *) PG_GETARG_POINTER(0);
+	BrinValues *column = (BrinValues *) PG_GETARG_POINTER(1);
+	Datum newval = PG_GETARG_DATUM(2);
+	bool		isnull = PG_GETARG_BOOL(3);
+	PG_RETURN_DATUM(gidx_brin_inclusion_add_value(bdesc, column, newval, isnull, 3));
+}
+
+Datum
+gidx_brin_inclusion_add_value(BrinDesc *bdesc, BrinValues *column, Datum newval, bool isnull, int ndims)
+{
+	GIDX * gidx_geom, *gidx_index;
+	int dims_to_copy, i;
+	if (isnull)
+	{
+		if (column->bv_hasnulls)
+			PG_RETURN_BOOL(false);
+
+		column->bv_hasnulls = true;
+		PG_RETURN_BOOL(true);
+	}
+	gidx_geom = gidx_new(4);
+	if(gserialized_datum_get_gidx_p(newval, gidx_geom) == LW_FAILURE){
+		elog(ERROR, "Error while extracting the gidx from the geom");
+	}
+	if (column->bv_allnulls)
+	{
+		column->bv_values[INCLUSION_UNION] = datumCopy((Datum) gidx_geom, false, GIDX_SIZE(ndims));
+		column->bv_values[INCLUSION_UNMERGEABLE] = BoolGetDatum(false);
+		column->bv_values[INCLUSION_CONTAINS_EMPTY] = BoolGetDatum(false);
+		column->bv_allnulls = false;
+		PG_RETURN_BOOL(true);
+	}
+	dims_to_copy = Min(ndims, GIDX_NDIMS(gidx_geom));
+	Assert(dims_to_copy == ndims);
+	gidx_index = (GIDX *) column->bv_values[INCLUSION_UNION];
+	for ( i = 0; i < dims_to_copy; i++ )
+	{
+		/* Adjust minimums */
+		GIDX_SET_MIN(gidx_index, i, Min(GIDX_GET_MIN(gidx_index,i),GIDX_GET_MIN(gidx_geom,i)));
+		/* Adjust maximums */
+		GIDX_SET_MAX(gidx_index, i, Max(GIDX_GET_MAX(gidx_index,i),GIDX_GET_MAX(gidx_geom,i)));
+	}
+	pfree(gidx_geom);
+	PG_RETURN_BOOL(false);
 }
