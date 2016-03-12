ea19fcc54b7bce608b08a6add6f5518e5c8559a1
Julien Rouhaud <julien.rouhaud@free.fr>
2016-03-12 19:41:31 +0100

    SPlit brin_nd to 2 files, and add HAVE_BRIN makefile var
    We now have brin_2d.c and brin_nd.d which shared postgis_brin.h
    
    Configure will detect if postgres has BRIN support. This support makes
    optionnal:
    
    - compile brin_*
    - related regression tests
    
    we could refactor every places that used POSTGIS_PGSQL_VERSION >= 95 to instead
    use HAVE_BRIN

--- postgis/brin_nd.c
+++ postgis/brin_nd.c
@@ -1,27 +1,4 @@
-#include "postgres.h"
-
-#if PG_VERSION_NUM >= 90500
-
-#include "fmgr.h"
-
-#include "../postgis_config.h"
-
-/*#define POSTGIS_DEBUG_LEVEL 4*/
-
-#include "liblwgeom.h"         /* For standard geometry types. */
-#include "lwgeom_pg.h"       /* For debugging macros. */
-
-#include <assert.h>
-#include <math.h>
-#include <float.h>
-#include <string.h>
-#include "access/brin_tuple.h"
-#include "utils/datum.h"
-#include "gserialized_gist.h"
-
-#define INCLUSION_UNION				0
-#define INCLUSION_UNMERGEABLE		1
-#define INCLUSION_CONTAINS_EMPTY	2
+#include "postgis_brin.h"
 
 /*
  * As we index geometries but store either a BOX2DF or GIDX according to the
@@ -33,54 +10,6 @@
  * by doing it this way, namely calling different functions through the
  * FunctionCallInvoke machinery for each heap tuple.
  */
-
-PG_FUNCTION_INFO_V1(geom2d_brin_inclusion_add_value);
-Datum
-geom2d_brin_inclusion_add_value(PG_FUNCTION_ARGS)
-{
-	BrinDesc   *bdesc = (BrinDesc *) PG_GETARG_POINTER(0); /* not needed */
-	BrinValues *column = (BrinValues *) PG_GETARG_POINTER(1);
-	Datum newval = PG_GETARG_DATUM(2);
-	bool		isnull = PG_GETARG_BOOL(3);
-	BOX2DF box_geom, *box_key;
-
-	if (isnull)
-	{
-		if (column->bv_hasnulls)
-			PG_RETURN_BOOL(false);
-
-		column->bv_hasnulls = true;
-		PG_RETURN_BOOL(true);
-	}
-
-	if (gserialized_datum_get_box2df_p(newval, &box_geom) == LW_FAILURE)
-		elog(ERROR, "Error while extracting the box2df from the geom");
-
-	/* if the recorded value is null, we just need to store the box2df */
-	if (column->bv_allnulls)
-	{
-		column->bv_values[INCLUSION_UNION] = datumCopy((Datum) &box_geom, false,
-				sizeof(BOX2DF));
-		column->bv_values[INCLUSION_UNMERGEABLE] = BoolGetDatum(false);
-		column->bv_values[INCLUSION_CONTAINS_EMPTY] = BoolGetDatum(false);
-		column->bv_allnulls = false;
-		PG_RETURN_BOOL(true);
-	}
-
-	/*
-	 * Otherwise, we need to enlarge the stored box2df to make it contains the
-	 * current geometry
-	 */
-	box_key = (BOX2DF *) column->bv_values[INCLUSION_UNION];
-
-	/* enlarge box2df */
-	box_key->xmin = Min(box_key->xmin, box_geom.xmin);
-	box_key->xmax = Max(box_key->xmax, box_geom.xmax);
-	box_key->ymin = Min(box_key->ymin, box_geom.ymin);
-	box_key->ymax = Max(box_key->ymax, box_geom.ymax);
-
-	PG_RETURN_BOOL(false);
-}
 
 PG_FUNCTION_INFO_V1(geom3d_brin_inclusion_add_value);
 Datum
@@ -144,5 +73,3 @@ geom3d_brin_inclusion_add_value(PG_FUNCTION_ARGS)
 
 	PG_RETURN_BOOL(false);
 }
-
-#endif
